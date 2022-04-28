086057f54cf0920d1cce5ade79a00b7a194c1e8e
unknown <jonathan.f.emery@uvm.edu>
2022-04-27 23:58:12 -0400

    Trying to fix missing tags.

--- artist_network.R
+++ artist_network.R
@@ -60,3 +60,82 @@ get_artist_network <- function(hist, seed = 1){
   
   return(vis_net)
 }
+
+
+#REFERENCE CODE
+hist <- lastfmR::get_scrobbles("jemery2016", "EST")
+hist <- hist |> 
+  mutate(date_num = ymd_hms(date) |> 
+           as_date() |> 
+           as.numeric()) |> 
+  as_tibble()
+
+artist_edge_df <- hist |> 
+  select(date, date_num, artist) |> 
+  #filter(date_num >= max(date_num) - 30) |> 
+  mutate(next_date = lag(date),
+         next_date_num = lag(date_num),
+         next_artist = lag(artist)) |> 
+  filter(artist != next_artist) |> 
+  group_by(artist, next_artist) |> 
+  summarise(edge_count = n(),
+            first_date = first(date),
+            .groups = "drop") |> 
+  arrange(desc(first_date)) |> 
+  as_tibble()
+
+artist_node_df <- hist |>
+  #filter(date_num >= max(date_num) - 30) |> 
+  group_by(artist) |> 
+  summarize(plays = n()) |> 
+  as_tibble()
+
+get_more_tags <- function(artist_node_df){
+  artist_node_df$tag <- NA
+  new_count <- 0
+  miss_index <- 1:nrow(artist_node_df)
+  continue <- TRUE
+  while(continue){
+    Sys.sleep(10)
+    old_count <- new_count
+    
+    tags <- lastfmR::get_tags(artist_node_df$artist[miss_index]) |>
+      group_by(artist) |>
+      summarize(tag = first(tag)) |>
+      as_tibble()
+    
+    artist_node_df <- artist_node_df |> 
+      left_join(tags, by = "artist") |> 
+      mutate(tag = coalesce(tag.x, tag.y)) |> 
+      select(-tag.x, -tag.y)
+    
+    miss_index <- which(is.na(artist_node_df$tag))
+    new_count <- length(miss_index)
+    continue <- new_count != old_count
+    print(old_count); print(new_count)
+  }
+  return(artist_node_df)
+}
+
+test <- get_more_tags(artist_node_df)
+
+#getting tags 
+tags <- lastfmR::get_tags(artist_node_df$artist) |>
+  group_by(artist) |>
+  summarize(tag = first(tag)) |>
+  as_tibble()
+
+artist_node_df <- artist_node_df |> 
+  left_join(tags, by = "artist")
+
+missing_tags <- artist_node_df |> 
+  filter(is.na(tag))
+
+tags <- lastfmR::get_tags(missing_tags$artist) |> 
+  group_by(artist) |>
+  summarize(tag = first(tag)) |>
+  as_tibble()
+ 
+
+missing_tags |> left_join(tags, by = "artist") |> View()
+
